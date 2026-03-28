bb9e34e603c9461c92c0ab3e2db757bce2a3f78d
misemi-ya <hashikankawatii@gmail.com>
2026-03-28 17:43:51 +0900

    Add files via upload

---
+++ pwa.js
@@ -0,0 +1,12 @@
+﻿(() => {
+  const isStandalone = window.matchMedia("(display-mode: standalone)").matches || window.navigator.standalone === true;
+  if (isStandalone) {
+    document.body.classList.add("is-standalone");
+  }
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker.register("./service-worker.js").catch(() => {});
+    });
+  }
+})();
